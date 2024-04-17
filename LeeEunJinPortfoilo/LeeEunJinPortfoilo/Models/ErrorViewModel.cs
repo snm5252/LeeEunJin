2a78da9f94ecefc9e52d0a60524e1c7651ae1fd2
eunjin.lee <lej@dlit.co.kr>
2024-04-17 11:14:52 +0900

    최초커밋

---
+++ LeeEunJinPortfoilo/LeeEunJinPortfoilo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeeEunJinPortfoilo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
